a084fad9791ee1bf196ade8318028cd20f1d8a51
Benoit Chesneau <bchesneau@gmail.com>
2026-01-26 16:04:08 +0100

    Use dirty NIF I/O schedulers for all I/O operations

--- c_src/iommap_nif.c
+++ c_src/iommap_nif.c
@@ -53,12 +53,12 @@ static int on_upgrade(ErlNifEnv *env, void **priv_data, void **old_priv_data,
  */
 static ErlNifFunc nif_funcs[] = {
     {"nif_open",     3, iommap_nif_open,     ERL_NIF_DIRTY_JOB_IO_BOUND},
-    {"nif_close",    1, iommap_nif_close,    0},
-    {"nif_pread",    3, iommap_nif_pread,    0},
-    {"nif_pwrite",   3, iommap_nif_pwrite,   0},
+    {"nif_close",    1, iommap_nif_close,    ERL_NIF_DIRTY_JOB_IO_BOUND},
+    {"nif_pread",    3, iommap_nif_pread,    ERL_NIF_DIRTY_JOB_IO_BOUND},
+    {"nif_pwrite",   3, iommap_nif_pwrite,   ERL_NIF_DIRTY_JOB_IO_BOUND},
     {"nif_sync",     2, iommap_nif_sync,     ERL_NIF_DIRTY_JOB_IO_BOUND},
     {"nif_truncate", 2, iommap_nif_truncate, ERL_NIF_DIRTY_JOB_IO_BOUND},
-    {"nif_advise",   4, iommap_nif_advise,   0},
+    {"nif_advise",   4, iommap_nif_advise,   ERL_NIF_DIRTY_JOB_IO_BOUND},
     {"nif_position", 1, iommap_nif_position, 0}
 };
 
